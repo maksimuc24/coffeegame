7b9db5c5f7bf6a0c25a557cab4afd7cc8c57c790
Maksym <maksimuc24web@gmail.com>
2015-07-10 01:52:39 +0300

    add vendor js files

--- server/dist/js/main-src.js
+++ server/dist/js/main-src.js
@@ -543,31 +543,6 @@
 
         angular
                 .module('coffeeGame')
-                .controller('GameCtrl', GameCtrl);
-
-        GameCtrl.$inject = ['$scope', '$rootScope', 'User'];
-
-        function GameCtrl($scope, $rootScope, User) {
-                $scope.game = {};
-
-                $rootScope.$on('gameStartEvent', function() {
-                        console.log('GameCtrl gameStartEvent');
-                        $scope.game.equipmentChooseFinished = true;
-                });
-
-                $rootScope.$on('userLogin', function(e, authUser) {
-                        $scope.user = new User(authUser);
-                        $scope.user.getBalance();
-                });
-        };
-})();
-
-(function() {
-        'use strict'
-
-
-        angular
-                .module('coffeeGame')
                 .controller('LoginCtrl', LoginCtrl);
 
         LoginCtrl.$inject = ['$scope', 'authenticationService', '$location'];
@@ -587,6 +562,31 @@
                                 $location.path('/');
                         });
                 }
+        };
+})();
+
+(function() {
+        'use strict'
+
+
+        angular
+                .module('coffeeGame')
+                .controller('GameCtrl', GameCtrl);
+
+        GameCtrl.$inject = ['$scope', '$rootScope', 'User'];
+
+        function GameCtrl($scope, $rootScope, User) {
+                $scope.game = {};
+
+                $rootScope.$on('gameStartEvent', function() {
+                        console.log('GameCtrl gameStartEvent');
+                        $scope.game.equipmentChooseFinished = true;
+                });
+
+                $rootScope.$on('userLogin', function(e, authUser) {
+                        $scope.user = new User(authUser);
+                        $scope.user.getBalance();
+                });
         };
 })();
 
