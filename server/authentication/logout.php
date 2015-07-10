79d506f4cb560aee66ed195fb6231014dd75fe8c
Maksym <maksimuc24web@gmail.com>
2015-07-10 16:00:14 +0300

    fix user logout and registration

--- server/authentication/logout.php
+++ server/authentication/logout.php
@@ -6,6 +6,8 @@ require_once(dirname(__FILE__).'/UserManager.php');
 
 $userManager = new UserManager();
 
+if(!isset($_SESSION['user_id']))
+	return;
 $userId = $_SESSION['user_id'];
 
 $userManager->RemoveHash($userId);
