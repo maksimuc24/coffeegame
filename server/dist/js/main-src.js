81c2ab2f2a4acaa81cde9073afeee50eb0033101
Maksym <maksimuc24web@gmail.com>
2015-07-13 23:54:21 +0300

    new .gif

--- server/dist/js/main-src.js
+++ server/dist/js/main-src.js
@@ -570,6 +570,22 @@
 
         angular
                 .module('coffeeGame')
+                .controller('globalSettingsCtrl', globalSettingsCtrl);
+
+        globalSettingsCtrl.$inject = ['$scope', '$rootScope', 'User'];
+
+        function globalSettingsCtrl($scope, $rootScope, User) { 
+
+                console.log('globalSettingsCtrl');
+        };
+})();
+
+(function() {
+        'use strict'
+
+
+        angular
+                .module('coffeeGame')
                 .controller('LoginCtrl', LoginCtrl);
 
         LoginCtrl.$inject = ['$scope', 'authenticationService', '$location'];
